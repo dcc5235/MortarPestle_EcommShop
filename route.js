91c72283774fa8a22bc5cc944a4113520d02d1cc
Dany Chheang <dany.chheang@gmail.com>
2021-03-08 15:05:54 -0600

    added router for products page to re-route to checkout page

---
+++ route.js
@@ -0,0 +1,43 @@
+
+// ROUTER (NEEDS CLEAN UP)
+window.onload = function () {
+  const view = document.getElementById('view');
+
+  const activeRoutes = Array.from(document.querySelectorAll('[route]'));
+  function navigate(event) {
+    let route = event.target.attributes[0].value;
+    let routeInfo = myFirstRouter.routes.filter(function (r) {
+      return r.path === route;
+    })[0];
+    if (!routeInfo) {
+      window.history.pushState({}, '', 'error')
+      window.location.pathname = './src/pages/checkout/checkout.html'
+    }
+  };
+
+  activeRoutes.forEach(function (route) {
+    route.addEventListener('click', navigate, false);
+  });
+};
+
+const Router = function (name, routes) {
+  return {
+    name: name,
+    routes: routes
+  }
+};
+
+const myFirstRouter = new Router('myFirstRouter', [
+  {
+    path: '/',
+    name: 'Route'
+  },
+  {
+    path: '/checkout',
+    name: 'Checkout'
+  },
+  {
+    path: '/products',
+    name: 'Products'
+  }
+]);
